ec49deaad35152bb497b256d98d223afe186c62a
Alexis Derail <alexisderail@MBP-de-Alexis.rezal-mdm.com>
2024-09-30 20:05:59 +0200

    premier commit

--- fibonacci.py
+++ fibonacci.py
@@ -2,11 +2,11 @@
 la suite de fibonacci
 """
 
-def fibo(n):
+def fibo_cached(n):
     """
     # une implémentation naïve et inefficace
     """
     if n <= 1:
         return n
     else:
-        return fibo(n-1) + fibo(n-2)
+        return fibo_cached(n-1) + fibo_cached(n-2)
